e0318da4a18a4a678f22ad4efbdadb3b2eec9906
tky823 <tky823.main+github@gmail.com>
2023-01-01 16:47:40 +0900

    [src] Add tex style

--- bss_visualize/__init__.py
+++ bss_visualize/__init__.py
@@ -1,6 +1,18 @@
+import matplotlib.pyplot as plt
+
 from .boxplot import boxplot_by_group
 from .plot import plot_by_elapsed_time, plot_by_iteration
 
 __version__ = "0.0.0"
 
-__all__ = ["boxplot_by_group", "plot_by_elapsed_time", "plot_by_iteration"]
+__all__ = ["set_tex_style", "boxplot_by_group", "plot_by_elapsed_time", "plot_by_iteration"]
+
+
+def set_tex_style():
+    params = {
+        "text.usetex": True,
+        "font.family": "serif",
+        "font.serif": ["Computer Modern Serif"],
+    }
+
+    plt.rcParams.update(params)
